9a23fb409bd246c008f822cc8413604243fc0939
Benoît Legat <benoit.legat@gmail.com>
2017-01-19 22:45:01 +0100

    Add tests

--- test/sumofsquares.jl
+++ test/sumofsquares.jl
@@ -6,15 +6,16 @@ sostest(s) = include(joinpath(polyhedra_test, "$s.jl"))
 
 sostest("solvers")
 
+sostest("certificate")
+
 sostest("motzkin")
-sostest("choi")
 
 # SOSTools demos
-sostest("sosdemo1")
+sostest("sospoly")
 sostest("sosdemo2")
 sostest("sosdemo3")
 sostest("sosdemo4")
-#sostest("sosdemo5")
-#sostest("sosdemo6")
-sostest("sosdemo9")
-sostest("sosdemo10")
+sostest("sosdemo5")
+sostest("sosdemo6")
+sostest("domain")
+sostest("sosmatrix")

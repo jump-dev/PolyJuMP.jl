6795b76adc2d2c1fa4b7b9a22987454072887e3c
Benoît Legat <benoit.legat@gmail.com>
2017-11-06 17:26:19 +0100

    Add initial MOI support

---
+++ src/macros.jl
@@ -0,0 +1,75 @@
+using JuMP
+import JuMP: validmodel, addtoexpr_reorder
+using Base.Meta
+
+export Poly, @set
+
+function JuMP.getvalue(t::AbstractTerm{<:JuMP.AbstractJuMPScalar})
+    getvalue(coefficient(t)) * monomial(t)
+end
+function JuMP.getvalue(p::AbstractPolynomialLike{<:JuMP.AbstractJuMPScalar})
+    polynomial(getvalue.(terms(p)), MultivariatePolynomials.SortedUniqState())
+end
+
+abstract type AbstractPoly end
+
+# x is a vector of monomials to be used to construct a polynomial variable
+# if MT is Gram, x represents the monomials of the form x^T Q x
+# if MT is Classic, it represents the monomials of the form a^T x
+# if MT is Default, it depends on whether the polynomials is constructed as nonnegative or not:
+# For a nonnegative polynomial, it corresponds to Gram, otherwise it corresponds to Classic.
+struct Poly{P, MT, MV} <: AbstractPoly
+    x::MV
+end
+Poly{P, MT}(x::MV) where {P, MT, MV} = Poly{P, MT, MV}(x)
+Poly{P}(x) where P = Poly{P, :Default}(x)
+Poly(x) = Poly{false}(x)
+
+function JuMP.variabletype(m::Model, p::AbstractPoly)
+    getpolymodule(m).polytype(m, p)
+end
+function cvarchecks(_error::Function, haslb::Bool, lowerbound::Number, hasub::Bool, hasfix::Bool, hasstart::Bool; extra_kwargs...)
+    for (kwarg, _) in extra_kwargs
+        _error("Unrecognized keyword argument $kwarg")
+    end
+    if hasfix || hasstart
+        _error("Polynomial variable declaration does not support the form ... == value.")
+    end
+    if haslb && hasub
+        _error("Polynomial variable declaration does not support the form lb <= ... <= ub. Use ... >= 0 and separate constraints instead.")
+    end
+    if haslb && !iszero(lowerbound)
+        _error("Polynomial variable declaration does not support the form ... >= lb with nonzero lb.")
+    end
+    if hasub
+        _error("Polynomial variable declaration does not support the form ... <= ub.")
+    end
+end
+function _warnbounds(_error, p::AbstractPoly, haslb, hasub) end
+function _warnbounds(_error, p::Poly{false}, haslb, hasub)
+    if haslb
+        _error("Free polynomial variable declaration does not support the form ... >= 0, use SOSPoly(x) instead of Poly(x) to create Sum of Squares polynomials. Note that SOSPoly(x) creates the polynomial x^T Q x with Q symmetric positive semidefinite while Poly(x) creates the polynomial a^T x so the meaning of the vector of monomial x changes from Poly to SOSPoly.")
+    end
+end
+function JuMP.constructvariable!(m::Model, p::AbstractPoly, _error::Function,
+                                 haslb::Bool, lowerbound::Number,
+                                 hasub::Bool, upperbound::Number,
+                                 hasfix::Bool, fixedvalue::Number,
+                                 binary::Bool, integer::Bool, name::AbstractString,
+                                 hasstart::Bool, start::Number; extra_kwargs...)
+    cvarchecks(_error, haslb, lowerbound, hasub, hasfix, hasstart; extra_kwargs...)
+    _warnbounds(_error, p, haslb, hasub)
+    getpolymodule(m).createpoly(m, p, binary, integer)
+end
+
+function JuMP.constructconstraint!(p::AbstractPolynomialLike, sense::Symbol)
+    PolyConstraint(sense == :(<=) ? -p : p, sense == :(==) ? ZeroPoly() : NonNegPoly())
+end
+
+function JuMP.constructconstraint!{PolyT<:AbstractPolynomialLike}(p::Union{PolyT, AbstractMatrix{PolyT}}, s)
+    PolyConstraint(p, s)
+end
+# there is already a method for AbstractMatrix in PSDCone in JuMP so we need a more specific here to avoid ambiguity
+function JuMP.constructconstraint!{PolyT<:AbstractPolynomialLike}(p::AbstractMatrix{PolyT}, s::PSDCone)
+    PolyConstraint(p, s)
+end
--- test/variable.jl
+++ test/variable.jl
@@ -14,13 +14,15 @@
     @test_throws ErrorException @variable m p >= 0 Poly(X)
     @test_throws ErrorException @variable(m, p3[2:3] >= 0, Poly(X))
 
-    function testvar(m, p, x, category=:Cont, vars=true)
+    function testvar(m, p, x, binary=false, integer=false, vars=true)
         @test isa(p, vars ? DynamicPolynomials.Polynomial{true,JuMP.Variable} : DynamicPolynomials.Polynomial{true,JuMP.AffExpr})
         @test p.x == x
         if vars
-            @test all(α -> m.colCat[α.col] == category, coefficients(p))
+            @test all(α -> haskey(m.variabletozeroone, JuMP.index(α)) == binary, coefficients(p))
+            @test all(α -> haskey(m.variabletointegrality, JuMP.index(α)) == integer, coefficients(p))
         else
-            @test all(α -> m.colCat[α.vars[1].col] == category, coefficients(p))
+            @test all(α -> haskey(m.variabletozeroone, JuMP.index(first(keys(α.terms)))) == binary, coefficients(p))
+            @test all(α -> haskey(m.variabletointegrality, JuMP.index(first(keys(α.terms)))) == integer, coefficients(p))
         end
     end
 
@@ -29,39 +31,39 @@
         @variable m p1[1:3] Poly(X)
         @test isa(p1, Vector{DynamicPolynomials.Polynomial{true,JuMP.Variable}})
         testvar(m, p1[1], X)
-        @variable(m, p2, Poly(X), category=:Int)
-        testvar(m, p2, X, :Int)
+        @variable(m, p2, Poly(X), integer=true)
+        testvar(m, p2, X, false, true)
         @variable(m, p3[2:3], Poly(X))
         @test isa(p3, JuMP.JuMPArray{DynamicPolynomials.Polynomial{true,JuMP.Variable},1,Tuple{UnitRange{Int}}})
         testvar(m, p3[2], X)
-        @variable(m, p4[i=2:3,j=i:4], Poly(X), category=:Bin)
-        testvar(m, p4[2,3], X, :Bin)
+        @variable(m, p4[i=2:3,j=i:4], Poly(X), binary=true)
+        testvar(m, p4[2,3], X, true)
 
         X = [x^2, y^2]
         @variable m p5[1:3] Poly(X)
         @test isa(p5, Vector{DynamicPolynomials.Polynomial{true,JuMP.Variable}})
         testvar(m, p5[1], X)
-        @variable(m, p6, Poly(X), category=:Int)
-        testvar(m, p6, X, :Int)
+        @variable(m, p6, Poly(X), integer=true)
+        testvar(m, p6, X, false, true)
     end
 
     @testset "FixedPolynomialBasis" begin
         m = Model()
-        @variable(m, p1, Poly(FixedPolynomialBasis([1 - x^2, x^2 + 2])), category=:Bin)
-        testvar(m, p1, monovec([x^2, 1]), :Bin, false)
+        @variable(m, p1, Poly(FixedPolynomialBasis([1 - x^2, x^2 + 2])), Bin)
+        testvar(m, p1, monovec([x^2, 1]), true, false, false)
         @variable(m, p2[1:2], Poly(FixedPolynomialBasis([1 - x^2, x^2 + 2])))
-        testvar(m, p2[1], monovec([x^2, 1]), :Cont, false)
+        testvar(m, p2[1], monovec([x^2, 1]), false, false, false)
         # Elements of the basis have type monomial
         @variable(m, p3[2:3], Poly(FixedPolynomialBasis([x, x^2])))
-        testvar(m, p3[2], monovec([x^2, x]), :Cont, false)
+        testvar(m, p3[2], monovec([x^2, x]), false, false, false)
         # Elements of the basis have type term
-        @variable(m, p4[1:2], Poly(FixedPolynomialBasis([1, x, x^2])), category=:Bin)
-        testvar(m, p4[1], monovec([x^2, x, 1]), :Bin, false)
+        @variable(m, p4[1:2], Poly(FixedPolynomialBasis([1, x, x^2])), Int)
+        testvar(m, p4[1], monovec([x^2, x, 1]), true, false, false)
         # Elements of the basis have type variable
-        @variable(m, p5[-1:1], Poly(FixedPolynomialBasis([x, y])), category=:Int)
-        testvar(m, p5[0], monovec([x, y]), :Int, false)
-        @variable(m, p6[-1:1], Poly(FixedPolynomialBasis([x])), category=:Int)
-        testvar(m, p6[0], monovec([x]), :Int, false)
+        @variable(m, p5[-1:1], Poly(FixedPolynomialBasis([x, y])), integer=true)
+        testvar(m, p5[0], monovec([x, y]), false, true, false)
+        @variable(m, p6[-1:1], Poly(FixedPolynomialBasis([x])), integer=true)
+        testvar(m, p6[0], monovec([x]), false, true, false)
     end
 end
 

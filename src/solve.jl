40865de26d3e2c8f6b4a88aba257a6ba75db08e9
Benoît Legat <benoit.legat@gmail.com>
2018-06-01 15:01:03 +0200

    Remove solve file

--- src/solve.jl
+++
@@ -1,6 +0,0 @@
-function solvehook(m::JuMP.Model; suppress_warnings=false)
-    for c in getpolyconstr(m)
-        c.delegate = addpolyconstraint!(m, c.p, c.set, c.domain; c.kwargs...)
-    end
-    solve(m, suppress_warnings=suppress_warnings, ignore_solve_hook=true)
-end

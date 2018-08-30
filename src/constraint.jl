926f7b82dcd96db483979f1a41eb9f6b5907174a
Benoît Legat <benoit.legat@gmail.com>
2018-08-30 09:42:32 +0200

    Updates for Julia v1.0 and JuMP master

--- src/constraint.jl
+++ src/constraint.jl
@@ -15,30 +15,41 @@ end
 abstract type ConstraintDelegate end
 const PolyConstraintRef{CD<:ConstraintDelegate} = ConstraintRef{Model, CD}
 
-function JuMP.addconstraint(m::Model, pc::PolyConstraint, name::String; domain::AbstractSemialgebraicSet=FullSpace(), basis=MonomialBasis, kwargs...)
+function JuMP.add_constraint(m::Model, pc::PolyConstraint, name::String;
+                             domain::AbstractSemialgebraicSet=FullSpace(),
+                             basis=MonomialBasis, kwargs...)
     delegate = addpolyconstraint!(m, pc.p, pc.set, domain, basis; kwargs...)
     JuMP.ConstraintRef(m, delegate, JuMP.ScalarShape())
 end
 
 getdelegate(c::PolyConstraintRef) = c.index
 getslack(c::PolyConstraintRef) = getslack(getdelegate(c))
-JuMP.resultdual(c::PolyConstraintRef) = JuMP.resultdual(getdelegate(c))
+JuMP.result_dual(c::PolyConstraintRef) = JuMP.result_dual(getdelegate(c))
 
 # Macro
-function JuMP.buildconstraint(_error::Function, p::AbstractPolynomialLike, s::MOI.EqualTo)
+function JuMP.build_constraint(_error::Function, p::AbstractPolynomialLike,
+                               s::MOI.EqualTo)
     PolyConstraint(p-s.value, ZeroPoly())
 end
-function JuMP.buildconstraint(_error::Function, p::AbstractPolynomialLike, s::MOI.GreaterThan)
+function JuMP.build_constraint(_error::Function, p::AbstractPolynomialLike,
+                               s::MOI.GreaterThan)
     PolyConstraint(p-s.lower, NonNegPoly())
 end
-function JuMP.buildconstraint(_error::Function, p::AbstractPolynomialLike, s::MOI.LessThan)
+function JuMP.build_constraint(_error::Function, p::AbstractPolynomialLike,
+                               s::MOI.LessThan)
     PolyConstraint(s.upper-p, NonNegPoly())
 end
 
-function JuMP.buildconstraint(_error::Function, p::Union{AbstractPolynomialLike, AbstractMatrix{<:AbstractPolynomialLike}}, s)
+function JuMP.build_constraint(_error::Function,
+                               p::Union{AbstractPolynomialLike,
+                                        AbstractMatrix{<:AbstractPolynomialLike}},
+                               s)
     PolyConstraint(p, s)
 end
-# there is already a method for AbstractMatrix in PSDCone in JuMP so we need a more specific here to avoid ambiguity
-function JuMP.buildconstraint(_error::Function, p::AbstractMatrix{<:AbstractPolynomialLike}, s::PSDCone)
+# there is already a method for AbstractMatrix in PSDCone in JuMP so we need a
+# more specific here to avoid ambiguity
+function JuMP.build_constraint(_error::Function,
+                               p::AbstractMatrix{<:AbstractPolynomialLike},
+                               s::PSDCone)
     PolyConstraint(p, NonNegPolyMatrix())
 end

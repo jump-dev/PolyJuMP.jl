163f77c9c3b9df890d34dd694005313835adef41
Benoît Legat <benoit.legat@gmail.com>
2019-05-02 14:52:27 +0200

    Implement dual for zero in algebraic set

--- src/zero_polynomial_in_algebraic_set_bridge.jl
+++ src/zero_polynomial_in_algebraic_set_bridge.jl
@@ -1,20 +1,29 @@
+using LinearAlgebra
+
 struct ZeroPolynomialInAlgebraicSetBridge{T, F <: MOI.AbstractVectorFunction,
                                           BT <: AbstractPolynomialBasis,
+                                          DT <: AbstractSemialgebraicSet,
                                           MT <: AbstractMonomial,
                                           MVT <: AbstractVector{MT}} <: MOIB.AbstractBridge
     zero_constraint::MOI.ConstraintIndex{F, ZeroPolynomialSet{FullSpace, BT, MT, MVT}}
+    domain::DT
+    monomials::MVT
 end
 
-function ZeroPolynomialInAlgebraicSetBridge{T, F, BT, MT, MVT}(model::MOI.ModelLike,
-                                                               f::MOI.AbstractVectorFunction,
-                                                               s::ZeroPolynomialSet{<:AbstractAlgebraicSet}) where {T, F, BT, MT, MVT}
+function ZeroPolynomialInAlgebraicSetBridge{T, F, BT, DT, MT, MVT}(model::MOI.ModelLike,
+                                                                   f::MOI.AbstractVectorFunction,
+                                                                   s::ZeroPolynomialSet{<:AbstractAlgebraicSet}) where {T, F <: MOI.AbstractVectorFunction,
+                                                                                                                        BT <: AbstractPolynomialBasis,
+                                                                                                                        DT <: AbstractSemialgebraicSet,
+                                                                                                                        MT <: AbstractMonomial,
+                                                                                                                        MVT <: AbstractVector{MT}}
     p = polynomial(collect(MOI.Utilities.eachscalar(f)), s.monomials)
     # FIXME convert needed because the coefficient type of `r` is `Any` otherwise if `domain` is `AlgebraicSet`
     r = convert(typeof(p), rem(p, ideal(s.domain)))
     zero_constraint = MOI.add_constraint(model, MOIU.vectorize(coefficients(r)),
                                          ZeroPolynomialSet(FullSpace(), s.basis,
                                                            monomials(r)))
-    return ZeroPolynomialInAlgebraicSetBridge{T, F, BT, MT, MVT}(zero_constraint)
+    return ZeroPolynomialInAlgebraicSetBridge{T, F, BT, DT, MT, MVT}(zero_constraint, s.domain, s.monomials)
 end
 
 
@@ -23,24 +32,23 @@ function MOI.supports_constraint(::Type{ZeroPolynomialInAlgebraicSetBridge{T}},
                                  ::Type{<:ZeroPolynomialSet{<:AbstractAlgebraicSet}}) where T
     return true
 end
-function MOIB.added_constraint_types(::Type{<:ZeroPolynomialInAlgebraicSetBridge{T, F, BT, MT, MVT}}) where {T, F, BT, MT, MVT}
+function MOIB.added_constraint_types(::Type{<:ZeroPolynomialInAlgebraicSetBridge{T, F, BT, DT, MT, MVT}}) where {T, F, BT, DT, MT, MVT}
     return [(F, ZeroPolynomialSet{FullSpace, BT, MT, MVT})]
 end
 function MOIB.concrete_bridge_type(::Type{<:ZeroPolynomialInAlgebraicSetBridge{T}},
                                    F::Type{<:MOI.AbstractVectorFunction},
-                                   ::Type{<:ZeroPolynomialSet{<:AbstractAlgebraicSet,
-                                                              BT, MT, MVT}}) where {T, BT, MT, MVT}
+                                   ::Type{<:ZeroPolynomialSet{DT, BT, MT, MVT}}) where {T, BT, DT<:AbstractAlgebraicSet, MT, MVT}
     G = MOI.Utilities.promote_operation(-, T, F, F)
-    return ZeroPolynomialInAlgebraicSetBridge{T, G, BT, MT, MVT}
+    return ZeroPolynomialInAlgebraicSetBridge{T, G, BT, DT, MT, MVT}
 end
 
 # Attributes, Bridge acting as an model
-function MOI.get(::ZeroPolynomialInAlgebraicSetBridge{T, F, BT, MT, MVT},
-                 ::MOI.NumberOfConstraints{F, ZeroPolynomialSet{FullSpace, BT, MT, MVT}}) where {T, F, BT, MT, MVT}
+function MOI.get(::ZeroPolynomialInAlgebraicSetBridge{T, F, BT, DT, MT, MVT},
+                 ::MOI.NumberOfConstraints{F, ZeroPolynomialSet{FullSpace, BT, MT, MVT}}) where {T, F, BT, DT, MT, MVT}
     return 1
 end
-function MOI.get(b::ZeroPolynomialInAlgebraicSetBridge{T, F, BT, MT, MVT},
-                 ::MOI.ListOfConstraintIndices{F, ZeroPolynomialSet{FullSpace, BT, MT, MVT}}) where {T, F, BT, MT, MVT}
+function MOI.get(b::ZeroPolynomialInAlgebraicSetBridge{T, F, BT, DT, MT, MVT},
+                 ::MOI.ListOfConstraintIndices{F, ZeroPolynomialSet{FullSpace, BT, MT, MVT}}) where {T, F, BT, DT, MT, MVT}
     return [b.zero_constraint]
 end
 
@@ -49,5 +57,19 @@ function MOI.delete(model::MOI.ModelLike, c::ZeroPolynomialInAlgebraicSetBridge)
     MOI.delete(model, c.zero_constraint)
 end
 
-# Dual and primal are not defined as the monomial vector may change so the
-# vector returned wouldn't make sense
+# TODO ConstraintPrimal
+
+# Let A be the linear map corresponding to A(p) = rem(p, ideal(set.domain))
+# We want to compute A*(μ) (where A* is the conjugate of A); see
+# slide 9 of https://www.youtube.com/watch?v=C8dHxJCUHYw.
+# That is, for every monomial of the basis `mono`, we want to compute
+# `⟨mono, A*(μ)⟩`. By definition of the conjugacy, we have `⟨mono, A*(μ)⟩ = ⟨A(mono), μ⟩`
+# so we can compute compute it by `⟨rem(mono, ideal(set.domain)), μ⟩`
+function MOI.get(model::MOI.ModelLike, attr::MOI.ConstraintDual,
+                 bridge::ZeroPolynomialInAlgebraicSetBridge)
+    dual = MOI.get(model, attr, bridge.zero_constraint)
+    set = MOI.get(model, MOI.ConstraintSet(), bridge.zero_constraint)
+    μ = measure(dual, set.monomials)
+    I = ideal(bridge.domain)
+    return [dot(rem(mono, I), μ) for mono in bridge.monomials]
+end
--- test/Tests/zero_polynomial_in_algebraic_set.jl
+++ test/Tests/zero_polynomial_in_algebraic_set.jl
@@ -1,6 +1,7 @@
 using Test
 using JuMP
 using SemialgebraicSets
+using MultivariateMoments
 using PolyJuMP
 using DynamicPolynomials
 
@@ -30,7 +31,13 @@ function zero_polynomial_in_algebraic_set_test(optimizer,
 
     @test dual_status(model) == MOI.FEASIBLE_POINT
     @test dual(UpperBoundRef(β)) ≈ -1.0 atol=atol rtol=rtol
-
+    μ = dual(cref)
+    @test μ isa AbstractMeasure{Float64}
+    @test length(moments(μ)) == 2
+    @test moment_value(moments(μ)[1]) ≈ -1.0 atol=atol rtol=rtol
+    @test monomial(moments(μ)[1]) == x
+    @test moment_value(moments(μ)[2]) ≈ -1.0 atol=atol rtol=rtol
+    @test monomial(moments(μ)[2]) == y
 
     F = MOI.VectorAffineFunction{Float64}
     S = PolyJuMP.ZeroPolynomialSet{typeof(@set x == 1),MonomialBasis,

4ca864efbdb1fabe4bf2e181dc6a98cafd82fe70
Benoît Legat <benoit.legat@gmail.com>
2020-09-18 16:51:33 +0200

    Add missing methods for custom MOI sets

--- src/zero_polynomial.jl
+++ src/zero_polynomial.jl
@@ -6,8 +6,12 @@ struct ZeroPolynomialSet{DT <: AbstractSemialgebraicSet,
     basis::Type{BT}
     monomials::MVT
 end
+MOI.dimension(set::ZeroPolynomialSet) = length(set.basis)
+Base.copy(set::ZeroPolynomialSet) = set
 
 # `x`-in-`PlusMinusSet(set)` iff `x`-in-`set` and `-x`-in-`set`.
 struct PlusMinusSet{ST <: MOI.AbstractVectorSet} <: MOI.AbstractVectorSet
     set::ST
 end
+MOI.dimension(set::PlusMinusSet) = MOI.dimension(set.set)
+Base.copy(set::PlusMinusSet) = set

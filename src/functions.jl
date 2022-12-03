0767705993ab793a47922a15f54323b658b1c5e7
Benoît Legat <benoit.legat@gmail.com>
2022-12-03 11:02:26 +0100

    Remove hack for old MOI version (#81)

--- src/functions.jl
+++
@@ -1,7 +0,0 @@
-# TODO remove this type piracy when we require MOI v0.9 as we need this fix: https://github.com/jump-dev/MathOptInterface.jl/pull/800
-function Base.:*(f::MOI.Utilities.ScalarLike, p::AbstractPolynomialLike)
-    return MultivariatePolynomials.multconstant(f, p)
-end
-function Base.:*(p::AbstractPolynomialLike, f::MOI.Utilities.ScalarLike)
-    return MultivariatePolynomials.multconstant(p, f)
-end

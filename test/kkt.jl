43d5545a5993cae4d5511abe2b1378152ab54a74
Benoît Legat <benoit.legat@gmail.com>
2023-06-29 10:39:03 +0200

    Fix format

--- test/kkt.jl
+++ test/kkt.jl
@@ -40,10 +40,7 @@ function test_algebraic(var, T, solver)
     )
     c2 = MOI.add_constraint(
         model,
-        PolyJuMP.ScalarPolynomialFunction(
-            o * x^2 + o * y^2 - o * t^(2),
-            vars,
-        ),
+        PolyJuMP.ScalarPolynomialFunction(o * x^2 + o * y^2 - o * t^(2), vars),
         MOI.LessThan(z),
     )
     MOI.set(model, MOI.ObjectiveSense(), MOI.MAX_SENSE)

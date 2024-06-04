411c8e30f163460288df0fc848c7733fb1b5b82f
Benoît Legat <benoit.legat@gmail.com>
2024-06-04 11:11:19 +0200

    Fix

--- src/SAGE/SAGE.jl
+++ src/SAGE/SAGE.jl
@@ -155,7 +155,7 @@ function PolyJuMP.bridges(
     F::Type{<:MOI.AbstractVectorFunction},
     ::Type{Cone{Signomials{Nothing}}},
 )
-    return [(SAGEBridge, PolyJuMP.coefficient_type_or_float(F))]
+    return Tuple{Type,Type}[(SAGEBridge, PolyJuMP.coefficient_type_or_float(F))]
 end
 
 include("bridges/age.jl")
@@ -164,7 +164,7 @@ function PolyJuMP.bridges(
     F::Type{<:MOI.AbstractVectorFunction},
     ::Type{Cone{Signomials{Int}}},
 )
-    return [(AGEBridge, PolyJuMP.coefficient_type_or_float(F))]
+    return Tuple{Type,Type}[(AGEBridge, PolyJuMP.coefficient_type_or_float(F))]
 end
 
 include("bridges/signomial.jl")
@@ -173,7 +173,7 @@ function PolyJuMP.bridges(
     F::Type{<:MOI.AbstractVectorFunction},
     ::Type{Cone{Polynomials{M}}},
 ) where {M}
-    return [(SignomialsBridge, PolyJuMP.coefficient_type_or_float(F))]
+    return Tuple{Type,Type}[(SignomialsBridge, PolyJuMP.coefficient_type_or_float(F))]
 end
 
 end
